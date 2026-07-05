3349a28e0d1fc23b8aa51b79539c84e779edd61b
Shakti Sankar Mishra <shaktimishra718@gmail.com>
2026-07-05 20:11:00 +0530

    Set up environment with necessary packages and dataset

---
+++ environment.py
@@ -0,0 +1,6 @@
+!pip install --upgrade pip setuptools wheel
+!pip install segmentation-models-pytorch
+!pip uninstall -y albumentations
+!pip install -U git+https://github.com/albumentations-team/albumentations
+!pip install --upgrade opencv-contrib-python
+!git clone https://github.com/parth1620/Human-Segmentation-Dataset-master.git
